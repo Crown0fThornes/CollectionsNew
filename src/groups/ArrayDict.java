132be4cb6d8eedb484e712406aa971449fd73f1a
Lincoln Edsall <lnclndsll@gmail.com>
2023-11-28 01:33:59 -0500

    no such key

--- src/groups/ArrayDict.java
+++ src/groups/ArrayDict.java
@@ -36,10 +36,12 @@ public class ArrayDict<K, V> implements Group<Pair<K, V>> {
 	 * Removes a Key, Value pair from this
 	 * @param x
 	 */
-	public void remove(Pair<K, V> x) {
+	public V remove(Pair<K, V> x) {
+		if (!this.has(x.key)) throw new IllegalArgumentException("No Such Key");
 		for (int i = 0; i < this.size; i++) 
 			if (this.array.get(i).equals(x))
-				this.array.remove(i);
+				return (V) this.array.remove(i).value;
+		return null;
 		
 	}
 	
@@ -50,6 +52,7 @@ public class ArrayDict<K, V> implements Group<Pair<K, V>> {
 	 * @return
 	 */
 	public V remove(K key) {
+		if (!this.has(key)) throw new IllegalArgumentException("No Such Key");
 		for (int i = 0; i < this.size; i++) 
 			if (this.array.get(i).key.equals(key))
 				return (V) this.array.remove(i).value;
@@ -76,6 +79,7 @@ public class ArrayDict<K, V> implements Group<Pair<K, V>> {
 	 * @return
 	 */
 	public V get(K key) {
+		if (!this.has(key)) throw new IllegalArgumentException("No Such Key");
 		for (int i = 0; i < this.size; i++)
 			if (this.array.get(i).key.equals(key))
 				return (V) this.array.remove(i).value;
